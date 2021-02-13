04b535091324038d993716eea8549a6cf7b54de7
RaymondLoranger <Raymond.Loranger@bell.net>
2021-02-13 09:06:48 -0500

    0.1.18

--- lib/islands/client/state.ex
+++ lib/islands/client/state.ex
@@ -11,7 +11,7 @@ defmodule Islands.Client.State do
   alias __MODULE__
   alias Islands.{Engine, Game, Player, PlayerID, Tally}
 
-  @default_options %{mode: :manual, pause: 0, basic: false}
+  @default_options %{mode: :manual, pause: 0}
   @genders [:f, :m]
   @modes [:manual, :auto]
   @pause_range 0..10_000
@@ -46,17 +46,23 @@ defmodule Islands.Client.State do
           mode: :manual | :auto,
           pause: 0..10_000,
           move: [String.codepoint() | non_neg_integer | String.t()],
-          tally: Tally.t() | nil
+          tally: Tally.t()
         }
 
-  @spec new(Game.name(), PlayerID.t(), Player.name(), atom, Keyword.t()) :: t
+  @spec new(
+          Game.name(),
+          PlayerID.t(),
+          Player.name(),
+          Player.gender(),
+          Keyword.t()
+        ) :: t
   def new(game_name, player_id, player_name, gender, options \\ [])
 
   def new(game_name, player_id, player_name, gender, options)
       when is_binary(game_name) and is_binary(player_name) and
              player_id in @player_ids and gender in @genders and
              is_list(options) do
-    %{mode: mode, pause: pause, basic: basic?} = parse(options)
+    %{mode: mode, pause: pause} = parse(options)
 
     %State{
       game_name: game_name,
@@ -66,7 +72,7 @@ defmodule Islands.Client.State do
       mode: mode,
       pause: pause,
       move: [],
-      tally: if(basic?, do: nil, else: Engine.tally(game_name, player_id))
+      tally: Engine.tally(game_name, player_id)
     }
   end
 
@@ -80,9 +86,6 @@ defmodule Islands.Client.State do
 
   @spec parse(Keyword.t(), map) :: map
   defp parse([], options), do: options
-
-  defp parse([{:basic, basic?} | rest], options) when is_boolean(basic?),
-    do: parse(rest, %{options | basic: basic?})
 
   defp parse([{:mode, mode} | rest], options) when mode in @modes,
     do: parse(rest, %{options | mode: mode})
